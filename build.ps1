6276f42aad9d1db5364e2f85ac2e4cda4e3f9fbc
Jim Speir <jimbo80982@gmail.com>
2020-09-20 19:17:18 +0100

    Almost there...

--- build.ps1
+++ build.ps1
@@ -2,4 +2,5 @@ $IMAGE_ID="ghcr.io/${Env:REPO_OWNER}/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
 docker tag "${Env:IMAGE_NAME}" "${Env:IMAGE_ID}:${Env:VERSION}"
+echo "docker push ${Env:IMAGE_ID}:${Env:VERSION}"
 docker push "${Env:IMAGE_ID}":"${Env:VERSION}"
