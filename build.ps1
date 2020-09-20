0bdf8a50b54e07159aa999bd2675a87941d4c192
Jim Speir <jimbo80982@gmail.com>
2020-09-20 19:32:10 +0100

    Potentially the scope needs to be different

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $IMAGE_ID="ghcr.io/${Env:REPO_OWNER}/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
-docker tag "${Env:IMAGE_NAME}" "${Env:IMAGE_ID}:${Env:VERSION}"
-echo "docker push ${Env:IMAGE_ID}:${Env:VERSION}"
-docker push "${Env:IMAGE_ID}":"${Env:VERSION}"
+docker tag "${Env:IMAGE_NAME}" "${IMAGE_ID}:${VERSION}"
+echo "docker push ${IMAGE_ID}:${VERSION}"
+docker push "${IMAGE_ID}":"${VERSION}"
