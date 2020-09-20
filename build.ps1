d474e432da7f18f0aa600f2ac485e45784d081ff
Jim Speir <jimbo80982@gmail.com>
2020-09-20 18:35:04 +0100

    Finalising

--- build.ps1
+++ build.ps1
@@ -1,10 +1,5 @@
 $IMAGE_ID="ghcr.io/${Env:REPO_OWNER}/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
-# Use Docker `latest` tag convention
-if($Env:VERSION -eq "master") { $Env:VERSION="latest"; }
-
-echo "TAG: ${IMAGE_ID}:${VERSION}"
-
 docker tag "${Env:IMAGE_NAME}" "${Env:IMAGE_ID}:${Env:VERSION}"
-docker push "${IMAGE_ID}":"${Env:VERSION}"
+docker push "${Env:IMAGE_ID}":"${Env:VERSION}"
