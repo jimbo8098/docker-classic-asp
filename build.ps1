1e0ce4d0ca6f505885e56bad1d70f81989a6b536
Jim Speir <jimbo80982@gmail.com>
2020-09-21 10:24:55 +0100

    Working around the docker push

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/","
 
 docker tag "${Env:IMAGE_NAME}" "${IMAGE_ID}:${VERSION}"
 echo "docker push ${IMAGE_ID}:${VERSION}"
-docker push "${IMAGE_ID}":"${VERSION}"
+docker push "${IMAGE_ID}"
