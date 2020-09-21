9ec2268ea0f27991de16f8fd76db68f003c127f3
Jim Speir <jimbo80982@gmail.com>
2020-09-21 11:16:30 +0100

    Changing context of the build so that it should be held in test machine's memory prior to the push

--- build.ps1
+++ build.ps1
@@ -2,5 +2,4 @@ $IMAGE_ID="ghcr.io/${Env:REPO_OWNER}/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
 docker tag "${Env:IMAGE_NAME}" "${IMAGE_ID}:${VERSION}"
-echo "docker push ${IMAGE_ID}:${VERSION}"
 docker push "${Env:IMAGE_NAME}":"${VERSION}"
