5a365d4e5dc73df3768c7289d907437cf0e88796
Jim Speir <jimbo80982@gmail.com>
2020-09-21 18:53:05 +0100

    changing variable format

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $IMAGE_ID="${Env:CR_USER}/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
-echo "${Env:CR_PAT}" | docker login $Env:CR_HOST -u ${{ secrets.CR_USER }} --password-stdin
+echo "${Env:CR_PAT}" | docker login $Env:CR_HOST -u ${Env:CR_USER} --password-stdin
 docker tag "${Env:IMAGE_NAME}" "${IMAGE_ID}:${VERSION}"
 docker push "${Env:IMAGE_NAME}:${VERSION}"
