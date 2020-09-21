81d716b91e3303e66ba77861d2b6cde8f6d792cd
Jim Speir <jimbo80982@gmail.com>
2020-09-21 17:12:51 +0100

    Echos

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 $IMAGE_ID="${Env:REPO_OWNER}/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
+echo "docker tag ${Env:IMAGE_NAME} ${IMAGE_ID}:${VERSION}"
+echo "docker push ${Env:IMAGE_NAME}:${VERSION}"
 docker tag "${Env:IMAGE_NAME}" "${IMAGE_ID}:${VERSION}"
-docker push -D "${Env:IMAGE_NAME}:${VERSION}"
+docker push "${Env:IMAGE_NAME}:${VERSION}"
