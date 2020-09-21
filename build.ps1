e2778d7ced3628a86f706521a0336e2fdae3a3d0
Jim Speir <jimbo80982@gmail.com>
2020-09-21 17:54:04 +0100

    Plaintext my user

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$IMAGE_ID="${Env:REPO_OWNER}/${Env:IMAGE_NAME}"
+$IMAGE_ID="jimbo8098/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
 echo "docker tag ${Env:IMAGE_NAME} ${IMAGE_ID}:${VERSION}"
