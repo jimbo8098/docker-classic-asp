f189d381566842c47e856513cf80f06bca7142b7
Jim Speir <jimbo80982@gmail.com>
2020-09-21 11:32:37 +0100

    Changing quotes around

--- build.ps1
+++ build.ps1
@@ -2,4 +2,6 @@ $IMAGE_ID="ghcr.io/${Env:REPO_OWNER}/${Env:IMAGE_NAME}"
 $VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
 
 docker tag "${Env:IMAGE_NAME}" "${IMAGE_ID}:${VERSION}"
-docker push "${Env:IMAGE_NAME}":"${VERSION}"
+
+echo "docker push ${Env:IMAGE_NAME}:${VERSION}"
+docker push "${Env:IMAGE_NAME}:${VERSION}"
