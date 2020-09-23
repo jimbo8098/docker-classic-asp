34bab30dd7f26e0085628e16e802c2df7504206d
Jim Speir <jimbo80982@gmail.com>
2020-09-23 17:09:33 +0100

    Removing testing

--- build.ps1
+++
@@ -1,7 +0,0 @@
-$IMAGE_ID="${Env:CR_USER}/${Env:IMAGE_NAME}"
-$VERSION=(($Env:GITHUB_REF) -replace "master","latest") -replace "refs/heads/",""
-
-echo "${Env:CR_PAT} | docker login -u ${Env:CR_USER} --password-stdin"
-echo "${Env:CR_PAT}" | docker login -u ${Env:CR_USER} --password-stdin
-docker tag "${Env:IMAGE_NAME}" "${IMAGE_ID}:${VERSION}"
-docker push "${Env:IMAGE_ID}:${VERSION}"
